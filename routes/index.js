7f2f225cf3241d49f9b0ae6a6b75182a2981a326
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-30 13:02:56 -0500

    added welcome page with register or login options

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const express= require('express');
 const router = express.Router();
 
 // create GET request
-router.get('/', (req, res) => res.send("Welcome"));
+router.get('/', (req, res) => res.render("welcome"));
  
 module.exports = router;
